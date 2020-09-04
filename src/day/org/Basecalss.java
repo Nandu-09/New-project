b078385646ade354519b980dbbdc5154767c6a44
Nandu-09 <nkrkpm555@gmail.com>
2020-09-04 16:27:21 +0530

    sample code

---
+++ src/day/org/Basecalss.java
@@ -0,0 +1,14 @@
+package day.org;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+ static WebDriver driver;
+public class Basecalss {
+
+public void openBrowser() {
+	// TODO Auto-generated method stub
+System.setProperty("webdriver.chorme.driver","C:\\Users\\ELCOT\\Downloads\\chromedriver_win32");
+WebDriver driver = new ChromeDriver();
+}
+
+driver.get()
+}
